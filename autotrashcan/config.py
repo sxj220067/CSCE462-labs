d8b8b0f237c5628370af5686fb58b99ac4176d32
Sungjun <sxj220067@tamu.edu>
2026-04-16 16:30:48 -0500

    motor direction

--- autotrashcan/config.py
+++ autotrashcan/config.py
@@ -36,12 +36,12 @@ CENTER_DEADZONE_PX = 40
 MOVE_SCALE = 1.0  # scalar for speed command
 MOTOR_MOCK = False
 MOTOR_DRIVER = "l298n"
-L298N_LEFT_ENABLE_PIN = 18
-L298N_LEFT_IN1_PIN = 23
-L298N_LEFT_IN2_PIN = 24
-L298N_RIGHT_ENABLE_PIN = 13
-L298N_RIGHT_IN3_PIN = 5
-L298N_RIGHT_IN4_PIN = 6
+L298N_LEFT_ENABLE_PIN = 13
+L298N_LEFT_IN1_PIN = 5
+L298N_LEFT_IN2_PIN = 6
+L298N_RIGHT_ENABLE_PIN = 18
+L298N_RIGHT_IN3_PIN = 23
+L298N_RIGHT_IN4_PIN = 24
 L298N_PWM_FREQUENCY = 1000
 MOTOR_FORWARD_DUTY = 0.65
 MOTOR_TURN_DUTY = 0.55
@@ -52,7 +52,7 @@ MOTOR_TURN_DUTY = 0.55
 # - Decrease MOTOR_TURN_DUTY if turns are too sharp, jerky, or unstable.
 # - Set LEFT_MOTOR_INVERTED or RIGHT_MOTOR_INVERTED to True if that side spins backward when forward is expected.
 # This project does not use a separate motor calibration file; these config values are the main adjustment points.
-LEFT_MOTOR_INVERTED = False
+LEFT_MOTOR_INVERTED = True
 RIGHT_MOTOR_INVERTED = False
 
 # visual / behavior flags
